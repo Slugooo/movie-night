a899e52609f93749262397c8b6eb797566324ac9
Slugooo <clancrusher9000@gmail.com>
2026-08-19 17:37:54 -0500

    Support inline trailer playback on iPhone

--- src/components/trailer-player.tsx
+++ src/components/trailer-player.tsx
@@ -2,5 +2,5 @@
 
 // A plain iframe keeps YouTube from mutating React-owned DOM during a veto.
 export function TrailerPlayer({ videoId }: { videoId: string; onEnded: () => void }) {
-  return <iframe className="trailer" src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0`} title="Movie trailer" allow="autoplay; encrypted-media; picture-in-picture" allowFullScreen />;
+  return <iframe className="trailer" src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&playsinline=1&rel=0`} title="Movie trailer" allow="autoplay; encrypted-media; picture-in-picture" allowFullScreen />;
 }
